4beca110564accb6782eb3035c186b8ca5a63692
ZebraMilk <zebramilkx@gmail.com>
2022-03-11 14:32:15 -0600

    Add structure for module support Begin to flesh out the functions and where they belong. Add basic style elements to show *something* on the page.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const Player = (name, token) => {
   // TODO return a player object with some methods on it like .place or .score
-}
+};
 
+// This is the internal gameBoard, 
 const gameBoard = (() => {
   // TODO initialize an arry in here that is private
-  const board = [null, null, null, null, null, null, null, null, null]
-  
+  const board = [null, null, null, null, null, null, null, null, null];
+
 
 
 
@@ -18,9 +19,13 @@ const gameBoard = (() => {
 
 })();
 
-const gameControl = (() => {
+const gameFlow = (() => {
   // Here goes things like turn tracking, checking for win/tie
+  // new game
 
+  // turn track variable
+
+  // 
 
   return {
     // TODO expose some stuff
@@ -28,6 +33,56 @@ const gameControl = (() => {
 
 
 })();
+
+// Grabs the user option fields and stores those valuables as variables
+// Goal is to pass this.playerToken into another module and rename it for easier use
+const userOptions = ((doc) => {
+  const playerTokenChoice = doc.getElementById("player-token-choice").value;
+
+  return {
+    playerToken: playerTokenChoice
+  }
+})(document);
+
+// This grabs DOM elements and does stuff to them
+const displayController = ((doc) => {
+
+  // grid stuff
+  const board = doc.querySelector(".game-board");
+  const clearGrid = () => {
+    while (board.lastElementChild) {
+      board.removeChild(board.lastElementChild);
+    }
+  };
+  const makeGrid = () => {
+    for (let index = 0; index < 9; index++) {
+      const newSquare = doc.createElement("div");
+      newSquare.classList.add("square", index);
+      board.appendChild(newSquare);
+    };
+  };
+  
+  // event listener?
+
+
+
+
+  return {
+    // QUESTION: why do I return the function object and not the function expression, makeGrid() here?
+    newGame: makeGrid
+  };
+})(document);
+
+displayController.newGame()
+
+
+
+
+
+
+
+
+
 
 const computerAI = (() => {
 
@@ -37,8 +92,7 @@ const computerAI = (() => {
   return {
 
   };
-
-})
+})();
 
 
 
