c5b1fce7e1a1edb87590f0a613bc0f4578615c59
ZebraMilk <zebramilkx@gmail.com>
2022-03-13 10:32:01 -0500

    Add more functionality Need to figure out how to alter one array, the gameBoard

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-const Player = (name, token) => {
+const playerFactory = (name, token) => {
   // TODO return a player object with some methods on it like .place or .score
   const playerName = name;
   const playerToken = token;
   // This stores the id values of the places on the board where this player has moved
   const playerMoves = [];
-
+  return {
+    playerName,
+    playerToken,
+    playerMoves
+  };
   // Do I need any functions here? What do I want the player to be able to do?
 };
 
 
 // This is the internal gameBoard, 
 const gameBoard = (() => {
+  "use strict";
   // TODO initialize an arry in here that is private
-  const boardArray = [null, null, null, null, null, null, null, null, null];
+  const _boardArray = [null, null, null, null, null, null, null, null, null]
+
   const resetBoardArray = () => {
-    boardArray = [null, null, null, null, null, null, null, null, null];
+    _boardArray = [null, null, null, null, null, null, null, null, null]
   };
 
+  const updateBoard = (index, value) => (_boardArray[index] = value);
+  const logBoard = () => console.log(_boardArray);
 
   return {
     // TODO return an object with the public stuff I want to expose
     resetBoardArray,
-    board: boardArray,
+    updateBoard,
+    logBoard
   };
 
 })();
@@ -29,6 +38,7 @@ const gameBoard = (() => {
 // Grabs the user option fields and stores those valuables as variables
 // Goal is to pass playerToken into another module for easier use
 const userOptions = ((doc) => {
+  "use strict";
   const playerTokenChoice = doc.getElementById("player-token-choice");
 
 
@@ -45,18 +55,19 @@ const displayController = ((doc) => {
   const board = doc.querySelector(".game-board");
   board.addEventListener("click", (e) => {
     e.stopPropagation();
-    gameBoard.board[e.target.id] = userOptions.playerTokenChoice.value;
+    gameBoard.updateBoard(e.target.id, userOptions.playerTokenChoice.value);
     e.target.innerText = userOptions.playerTokenChoice.value;
   });
-  
 
 
-  const clearGrid = () => {
+
+  const _clearGrid = () => {
     while (board.lastElementChild) {
       board.removeChild(board.lastElementChild);
     }
   };
-  const makeGrid = () => {
+
+  const _makeGrid = () => {
     for (let index = 0; index < 9; index++) {
       const newSquare = doc.createElement("div");
       newSquare.classList.add("square");
@@ -65,12 +76,23 @@ const displayController = ((doc) => {
     };
   };
 
+  const newGrid = () => {
+    _clearGrid();
+    _makeGrid();
+    gameBoard.resetBoardArray;
+  };
+
+  const newGameBtn = doc.querySelector(".new-game");
+  newGameBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    newGrid();
+  });
+
   // event listeners?
 
   return {
     // QUESTION: why do I return the function object and not the function expression, makeGrid() here?
-    makeGrid: makeGrid,
-    clearGrid: clearGrid
+    newGrid
   };
 })(document);
 
@@ -88,38 +110,32 @@ const computerAI = (() => {
   };
 })();
 
-const gameFlow = ((doc) => {
+const gameFlow = (() => {
+  "use strict"
   // Here goes things like turn tracking, checking for win/tie
   let currentTurn;
   // new game
   const newGame = () => {
     // Clear the board ans set up a new one
-    displayController
-      .clearGrid()
-      .makeGrid();
+    displayController.newGrid();
     // Make player1 with the user choice, player2 with the other choice
-    const player1 = Player(player1, userOptions.playerTokenChoice.value);
-    const player2 = Player(player2, (userOptions.playerTokenChoice.value === "X" ? "O" : "X"));
+    const player1 = playerFactory(player1, userOptions.playerTokenChoice.value);
+    const player2 = playerFactory(player2, (userOptions.playerTokenChoice.value === "X" ? "O" : "X"));
     // Return the player whose turn is first
     currentTurn = (player1.token === "X" ? player1 : player2);
   };
   // turn track variable
   let changeTurn = () => currentTurn === player1 ? player2 : player1;
   // check game state for tie/win
-  const newGameButton = doc.querySelector(".new-game");
-  newGameButton.addEventListener("click", newGame);
 
   return {
     // TODO expose some stuff
     newGame,
-    changeTurn,
+    changeTurn
   };
 
 
-})(document);
+})();
 
 
-
-
-
-
+displayController.newGrid();
